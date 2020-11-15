6390645ebdd5c43ba537380bdb90544013c290ed
eseshinpu <eseshinpu.com>
2020-11-15 22:48:59 +0900

    [fix] inference.

--- src/inference.py
+++ src/inference.py
@@ -2,12 +2,15 @@ from pathlib import Path
 
 import cv2
 import torch
+from addict import Dict
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 from sklearn.metrics import (confusion_matrix, accuracy_score, precision_score,
                              recall_score, f1_score)
 from pytorch_lightning.trainer import seed_everything
 
 from pl_module import LightningModuleInference
+from dataset import ImageDataset
 from utils.factory import get_transform
 from utils.util import get_parser, read_yaml
 
@@ -34,29 +37,28 @@ def inference(cfg):
     # define transform
     transform = get_transform(cfg.transform.val)
 
-    test_root_path = Path(TEST_ROOT_PATH)
+    # define Dataset and Dataloader
+    dataset_cfg = Dict({"root": TEST_ROOT_PATH})
+    dataset = ImageDataset(dataset_cfg, transform)
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=cfg.data.dataloader.batch_size // 2,
+        num_workers=cfg.data.dataloader.num_workers,
+        shuffle=cfg.data.dataloader.shuffle,
+    )
+
     output_path = Path(OUTPUT_PATH)
     output_path.mkdir(parents=True, exist_ok=True)
 
-    # search test images
-    img_paths = [
-        p for p in list(test_root_path.glob("*/*"))
-        if p.suffix in TARGET_EXTENSION
-    ]
-
     labels, predicts = [], []
-    for img_path in tqdm(img_paths):
-        cls_name = img_path.parent.name
-        org_img = cv2.imread(str(img_path))
-        if org_img is None:
-            raise Exception("NotFound Image: {}".format(str(img_path)))
-        img = transform(image=org_img)["image"]
-        input = img.unsqueeze(0).to(device, non_blocking=True)
+    for imgs, targets in tqdm(dataloader):
+        input = imgs.to(device, non_blocking=True)
         outputs = pl_module(input)
         _, predicted_indexes = torch.max(outputs.data, 1)
-        predict_idx = predicted_indexes.cpu().numpy()
-        labels.append(str(cls_name))
-        predicts.append(str(predict_idx[0]))
+        targets = targets.cpu().numpy().tolist()
+        predict_idx = predicted_indexes.cpu().numpy().tolist()
+        labels.extend(targets)
+        predicts.extend(predict_idx)
     calc_eval(labels, predicts)
 
 
