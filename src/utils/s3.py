1e2033f820bf8ab056a20efe8d0386733b8c54c8
eseshinpu <ko4666@gmail.com>
2021-03-14 15:18:48 +0000

    [fix] s3.

--- src/utils/s3.py
+++ src/utils/s3.py
@@ -1,14 +1,14 @@
-from typing import Any
+from typing import Any, Callable
 import gorilla
 import fsspec
 
-def apply_gorrila(function: Any, module: Any):
+
+def apply_gorrila(function: Callable, module: Any):
     patch = gorilla.Patch(
-        module, 
-        function.__name__, 
-        function, 
-        settings=gorilla.Settings(allow_hit=True))
+        module, function.__name__, function, settings=gorilla.Settings(allow_hit=True)
+    )
     gorilla.apply(patch)
+
 
 def setup_endpoint(endpoint_url: str):
     def filesystem(protocol, **storage_options):
@@ -16,13 +16,15 @@ def setup_endpoint(endpoint_url: str):
             storage_options["client_kwargs"] = {
                 "endpoint_url": endpoint_url,
             }
-        original = gorilla.get_original_attribute(fsspec, 'filesystem')
+        original = gorilla.get_original_attribute(fsspec, "filesystem")
         return original(protocol, **storage_options)
+
     def open_files(*args, **kwargs):
         kwargs["client_kwargs"] = {
-                "endpoint_url": endpoint_url,
+            "endpoint_url": endpoint_url,
         }
-        original = gorilla.get_original_attribute(fsspec.core, 'open_files')
+        original = gorilla.get_original_attribute(fsspec.core, "open_files")
         return original(*args, **kwargs)
+
     apply_gorrila(filesystem, fsspec)
     apply_gorrila(open_files, fsspec.core)
