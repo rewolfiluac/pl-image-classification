b1bc44b110ff76350fef34903fe94fa9b2d1ba5f
eseshinpu <ko4666@gmail.com>
2021-03-14 15:50:16 +0000

    Auto Commit. (Before the experiment)

--- make_env.sh
+++ make_env.sh
@@ -2,9 +2,4 @@
 cat <<EOT > .env
 UID=`id -u`
 GID=`id -g`
-MINIO_ACCESS_KEY="minio-access-key"
-MINIO_SECRET_KEY="minio-secret-key"
-MLFLOW_S3_ENDPOINT_URL="http://minio:9000"
-AWS_ACCESS_KEY_ID="minio-access-key"
-AWS_SECRET_ACCESS_KEY="minio-access-key"
 EOT
