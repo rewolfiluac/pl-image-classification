873b8073529fc588164254f51a0f56ebeec4d41b
eseshinpu <ko4666@gmail.com>
2021-03-14 15:39:35 +0000

    [fix] quick fix.

--- make_env.sh
+++ make_env.sh
@@ -2,4 +2,9 @@
 cat <<EOT > .env
 UID=`id -u`
 GID=`id -g`
+MINIO_ACCESS_KEY="minio-access-key"
+MINIO_SECRET_KEY="minio-secret-key"
+MLFLOW_S3_ENDPOINT_URL="http://minio:9000"
+AWS_ACCESS_KEY_ID=$MINIO_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY=$MINIO_SECRET_KEY
 EOT
