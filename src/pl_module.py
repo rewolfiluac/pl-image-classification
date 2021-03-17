1ba1421491bc546d0a3253adc704f880e787238c
eseshinpu <ko4666@gmail.com>
2021-03-17 14:27:19 +0000

    Auto Commit. (Before the experiment)

--- src/pl_module.py
+++ src/pl_module.py
@@ -27,23 +27,23 @@ class LightningModuleReg(pl.LightningModule):
         y_hat = self(x)
         loss = self.loss(y_hat, y)
         correct, labels_size = self.__calc_correct(y_hat, y)
-        return {
-            'val_loss': loss,
-            "val_corr": correct,
-            "labels_size": labels_size}
+        return {"val_loss": loss, "val_corr": correct, "labels_size": labels_size}
 
     def validation_epoch_end(self, outputs):
-        val_loss_mean = torch.stack([x['val_loss'] for x in outputs]).mean()
-        labels_size = np.array([x['labels_size'] for x in outputs]).sum()
-        val_acc = np.array([x['val_corr']
-                            for x in outputs]).sum() / labels_size
-        metrics = {'val_loss_mean': float(val_loss_mean.cpu().numpy()),
-                   'val_acc': float(val_acc)}
-        self.log_dict(metrics)
+        val_loss_mean = torch.stack([x["val_loss"] for x in outputs]).mean()
+        labels_size = np.array([x["labels_size"] for x in outputs]).sum()
+        val_acc = np.array([x["val_corr"] for x in outputs]).sum() / labels_size
+        metrics = {
+            "val_loss_mean": float(val_loss_mean.cpu().numpy()),
+            "val_acc": float(val_acc),
+        }
+        self.logger.log_metrics(metrics, step=self.current_epoch)
+        # self.log_metrics(metrics, step=self.current_epoch)
 
     def configure_optimizers(self):
         optimizer, scheduler = get_optimizer(
-            cfg=self.cfg.optimizer, model_params=self.net.parameters())
+            cfg=self.cfg.optimizer, model_params=self.net.parameters()
+        )
         return [optimizer], [scheduler]
 
     def __calc_correct(self, outputs, labels):
