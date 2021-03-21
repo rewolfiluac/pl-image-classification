0f0e6519b560081e4c62d57c8576aedcee17fe2e
eseshinpu <ko4666@gmail.com>
2021-03-21 14:46:00 +0000

    Auto Commit. (Before the experiment)

--- src/train.py
+++ src/train.py
@@ -58,7 +58,8 @@ def train(cfg):
 def run(cfg: DictConfig):
     seed_everything(seed=cfg.general.seed)
 
-    mlflow.set_tracking_uri(cfg.server.mlflow_uri)
+    print(cfg.server.mlflow_uri)
+    mlflow.set_tracking_uri(cfg.defaults.server.mlflow_uri)
     mlflow.pytorch.autolog()
     artifacts_omegaconf(cfg)
     train(cfg)
